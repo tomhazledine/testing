57c66daa364c9e4ce86ec8768560e31b1ec3d746
Thomas Hazledine <tom.hazledine@headforwards.com>
2017-08-23 12:06:40 +0100

    code cleanup

--- app.js
+++ app.js
@@ -22,39 +22,8 @@ function build_largest_number(array){
 
     // Pre-join numbers that start with the same digit but one ends in zero...???
 
-    // console.log(sorted_array);
-
     // Invert the order of the sorted array.
     sorted_array.reverse();
-
-    // var leading_numbers = [];
-    // var temp_objects = [];
-    // var max_length = 0;
-
-
-
-
-    // for (var i = 0; i < array.length; i++) {
-    //     var temp_object = {};
-    //     var array_of_characters = array[i].toString().split('');
-
-    //     if ( array_of_characters.length > max_length) {
-    //         max_length = array_of_characters.length;
-    //     }
-
-    //     leading_numbers.push(parseInt(array_of_characters[0]));
-    //     temp_object.leading_number = array_of_characters[0];
-    //     temp_object.digit_count = array_of_characters.length;
-    //     temp_object.original_number = array[i];
-    //     temp_objects.push(temp_object);
-    // }
-
-    // for (var i = 0; i < sorted_array.length; i++) {
-    //     var array_of_characters = sorted_array[i].toString().split('');
-
-    // }
-    // console.log(temp_objects);
-    // console.log(max_length);
 
     // Join the sorted array to get our result.
     var result = parseInt(sorted_array.join(''));
@@ -69,10 +38,19 @@ function build_largest_number(array){
 }
 
 
+/**
+ * The following functions are baby-steps toward actually solving the
+ * problem fully. Currently it doesn't work for numbers that start with
+ * the same digit where one of them has multiple digits that are lower
+ * than the first digit (e.g. `5` and `50`).
+ */
+
 function bubble_sort(array) {
     var n = array.length;
     for (var i = n - 1; i >= 0; i--) {
         for (var j = 1; j <= i; j++) {
+
+            // Extra case for our specific problem
             var a = array[j - 1];
             var b = array[j]
             var a_1 = get_first_character(a);
@@ -85,6 +63,7 @@ function bubble_sort(array) {
             
             } else if (array[j - 1] > array[j]) {
                 
+                // Normal bubble sort
                 var temp = array[j - 1];
                 array[j - 1] = array[j];
                 array[j] = temp;
